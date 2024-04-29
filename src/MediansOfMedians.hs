20aac0aba39cb1eef7db562676e7f625623e3f43
Frank Staals <frank@fstaals.net>
2024-04-29 17:56:06 +0200

    some renamign

--- src/MediansOfMedians.hs
+++ src/MediansOfMedians.hs
@@ -1,12 +1,13 @@
-module MyLib
+module MediansOfMedians
   ( median
   , select
   , selectWith
   ) where
 
+import           Data.Functor ((<&>))
 import qualified Data.List as List
 
-import Debug.Trace
+import           Debug.Trace
 --------------------------------------------------------------------------------
 
 data Median a = UniqueMedian !a
@@ -14,19 +15,19 @@ data Median a = UniqueMedian !a
               deriving (Show,Read,Eq)
 
 -- | Returns the median element
-median    :: (Ord a, Show a) => [a] -> Maybe (Median a)
+median    :: Ord a => [a] -> Maybe (Median a)
 median xs = case xs of
               [] -> Nothing
               _  -> let n = length xs
                     in case n `quotRem` 2 of
                          (h,1) -> UniqueMedian <$> selectWith n (h+1) xs
-                         (h,_) -> let Just m  = selectWith n h xs
-                                      m' = List.minimum $ tail [y | y <- xs, y >= m ]
-                                      -- this is safe since: y=m will at least satisfy y
-                                      -- >= m moreover, since we computed the lower
-                                      -- median, there must also be at least be another element
-                                      -- that is ast least m.
-                                  in Just $ DoubleMedian m m'
+                         (h,_) -> selectWith n h xs <&> \m ->
+                                    let m' = List.minimum $ tail [y | y <- xs, y >= m ]
+                                        -- this is safe since: y=m will at least satisfy y
+                                        -- >= m moreover, since we computed the lower
+                                        -- median, there must also be at least be another element
+                                        -- that is ast least m.
+                                    in DoubleMedian m m'
 
 -- | returns the i^th smallest element from the list. (The smallest element being the
 -- 0^th).
@@ -35,11 +36,10 @@ median xs = case xs of
 -- prop> select i xs == Just (sort xs !! i)
 --
 -- \(O(n)\)
-select      :: (Ord a, Show a) => Int -> [a] -> Maybe a
+select      :: Ord a => Int -> [a] -> Maybe a
 select i xs = selectWith (length xs) i xs
 
-selectWith        :: (Ord a, Show a) => Int -> Int -> [a] -> Maybe a
-
+selectWith        :: Ord a => Int -> Int -> [a] -> Maybe a
 selectWith n i xs = case map medianOfFive <$> partitionIntoFives xs of
                       (rest, [])      -> selectSmall i $ List.sort rest
                       (rest, medians) ->
@@ -65,11 +65,6 @@ partitionWith m = foldr (\x (smallers,eqs,largers) -> case x `compare` m of
                                                         GT -> (smallers,eqs,x:largers)
                         ) ([],[],[])
 
--- maximum' :: Ord a => [a] -> Maybe a
--- maximum' = \case
---   []   -> Nothing
---   x:xs -> Just $ List.foldl' max x xs
-
 -- | Just give me the i^th value
 selectSmall    :: Int -> [a] -> Maybe a
 selectSmall i = \case
@@ -83,7 +78,6 @@ partitionIntoFives :: [a] -> ( [a]   -- ^ the remainder
                              )
 partitionIntoFives xs = case xs of
   []              -> (xs, [])
-  [_]             -> (xs, [])
   [_]             -> (xs, [])
   [_,_]           -> (xs, [])
   [_,_,_]         -> (xs, [])
--- test/Main.hs
+++ test/Main.hs
@@ -1,7 +1,7 @@
 module Main (main) where
 
 import qualified Data.List as List
-import           MyLib
+import           MediansOfMedians
 import           Test.Hspec
 import           Test.Hspec.QuickCheck
 import           Test.QuickCheck
@@ -9,21 +9,14 @@ import           Test.QuickCheck
 --------------------------------------------------------------------------------
 
 main :: IO ()
-main = do let xs = [0,0,0,-1,-1]
-                -- [-1,-1,0] -- [1,43,32,5,4,352,52,54,1,2,100,343]
-              n  = length xs
-          print n
-          print $ List.sort xs !! 2
-          print $ selectWith n 2 xs
-          -- print $ median xs
-          -- print $ partitionIntoFives xs
-          hspec spec
+main = hspec spec
 
+spec :: Spec
 spec = do describe "select" $ do
             prop "select same as sorting" $
               \(NonNegative i) (xs :: [Int]) ->
                 select i xs === safeSortingSelect i xs
 
-
+safeSortingSelect :: Ord a => Int -> [a] -> Maybe a
 safeSortingSelect i xs | 0 <= i && i < length xs = Just $ List.sort xs !! i
                        | otherwise               = Nothing
